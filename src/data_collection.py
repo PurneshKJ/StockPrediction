80852cbddb3257998dd4b9883acb68dae080ee41
PurneshKJ <2582863k@student.gla.ac.uk>
2022-01-14 20:45:07 +0000

    Added binanceDB class for DB operations.

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,56 +1,18 @@
 import config
-import pandas as pd
-import numpy as np
-from stockstats import StockDataFrame
-import mysql.connector
+# import pandas as pd
+# import numpy as np
+# from stockstats import StockDataFrame
+import binanceDB
 # import pymysql
 from binance import Client
-from binance import ThreadedWebsocketManager
-from binance import ThreadedDepthCacheManager
+# from binance import ThreadedWebsocketManager
+# from binance import ThreadedDepthCacheManager
 
 binance_api_key = config.API_KEY
 binance_api_secret = config.API_SECERT
 binance_client = Client(binance_api_key, binance_api_secret)
 
-class BinanceDB():
-    def __init__(self):
-
-        self.conn = mysql.connector.connect(host=config.ENDPOINT, 
-                                    user=config.USER, 
-                                    passwd=config.AWS_RDS_TOKEN)
-                                    # port=config.PORT, 
-                                    # database=config.DBNAME)
-        self.cursor = self.conn.cursor(buffered=True)
-        self.cursor.execute('''DROP DATABASE BINANCE''')
-        self.cursor.execute('''CREATE DATABASE BINANCE''')
-        self.cursor.execute('''USE BINANCE''')
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS STOCKDATA(
-                            timestamp INTEGER NOT NULL PRIMARY KEY,
-                            open REAL, high REAL, low REAL, close REAL,
-                            volume REAL,  macd REAL, rsi_6 REAL )''' )
-                            # macdsignal REAL, macdhist REAL,  rsi_slowd REAL,
-                            # n_trades INTEGER,obv REAL, atr REAL, ema200 REAL, sar REAL
-
-        self.cursor.execute("Select * from STOCKDATA")
-        headers = [i[0] for i in self.cursor.description]
-        print(headers)
-
-    def insert_data(self, timestamp:int, open:float, high:float, low:float, close:float, volume:float, macd:float, rsi_6:float):
-        self.cursor.execute('''INSERT INTO STOCKDATA VALUES (%s, %s, %s, %s, %s, %s, %s, %s)''',(timestamp, open, high, low, close, volume, macd, rsi_6))
-        return
-
-    def getAllStocks(self):
-        self.cursor.execute('''SELECT * FROM STOCKDATA''')
-        result = self.cursor.fetchall()
-        return result
-
-    def __del__(self):
-        self.conn.close()
-        print("CONNCECTION CLOSED.")
-
-
-
-BDB = BinanceDB()
+BDB = binanceDB.BinanceDB()
 
 BDB.insert_data(1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
 
