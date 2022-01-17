dd834b239cafc509e4d0fc1e40bb07706cfe36dc
mshaji123 <90268737+mshaji123@users.noreply.github.com>
2022-01-17 10:37:10 +0000

    Added code for live streaming of data, updated Database

--- src/binanceDB.py
+++ src/binanceDB.py
@@ -10,17 +10,17 @@ class BinanceDB():
                                     # port=config.PORT, 
                                     # database=config.DBNAME)
         self.cursor = self.conn.cursor(buffered=True)
-        self.cursor.execute('''DROP DATABASE BINANCE''')
-        self.cursor.execute('''CREATE DATABASE BINANCE''')
+        # self.cursor.execute('''DROP DATABASE BINANCE''')
+        # self.cursor.execute('''CREATE DATABASE BINANCE''')
         self.cursor.execute('''USE BINANCE''')
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS STOCKDATA(
-                            timestamp INTEGER NOT NULL PRIMARY KEY,
+                            timestamp BIGINT NOT NULL PRIMARY KEY,
                             open REAL, high REAL, low REAL, close REAL,
                             volume REAL,  macd REAL, rsi_6 REAL )''' )
                             # macdsignal REAL, macdhist REAL,  rsi_slowd REAL,
                             # n_trades INTEGER,obv REAL, atr REAL, ema200 REAL, sar REAL
 
-        self.cursor.execute("Select * from STOCKDATA")
+        self.cursor.execute('''Select * from STOCKDATA''')
         headers = [i[0] for i in self.cursor.description]
         print(headers)
 
@@ -32,6 +32,12 @@ class BinanceDB():
         self.cursor.execute('''SELECT * FROM STOCKDATA''')
         result = self.cursor.fetchall()
         return result
+
+    def deleteFirstRow(self):
+        self.cursor.execute('''DELETE FROM STOCKDATA ORDER BY timestamp limit 1''')
+    
+    def commit(self):
+        self.conn.commit()
 
     def __del__(self):
         self.conn.close()
--- src/data_collection.py
+++ src/data_collection.py
@@ -1,66 +1,99 @@
 import config
-# import pandas as pd
-# import numpy as np
-# from stockstats import StockDataFrame
+import pandas as pd
+import numpy as np
 import binanceDB
 # import pymysql
 from binance import Client
-# from binance import ThreadedWebsocketManager
+from binance import ThreadedWebsocketManager, AsyncClient
+from stockstats import StockDataFrame
 # from binance import ThreadedDepthCacheManager
 
-binance_api_key = config.API_KEY
-binance_api_secret = config.API_SECERT
-binance_client = Client(binance_api_key, binance_api_secret)
-
+klines_df = None
 BDB = binanceDB.BinanceDB()
 
-BDB.insert_data(1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+def get_historical_data(binance_client):
+    global klines_df
 
-results = BDB.getAllStocks()
+    print ("Collecting historical data")
+    klines = binance_client.get_historical_klines (
+                    config.SYMBOL,
+                    AsyncClient.KLINE_INTERVAL_30MINUTE,
+                    config.START_DATE,
+                    config.END_DATE)
 
-print(results)
+    klines_np = np.array(klines).astype('float64')
 
-# klines = binance_client.get_historical_klines (
-#                 config.SYMBOL,
-#                 Client.KLINE_INTERVAL_30MINUTE,
-#                 config.START_DATE,
-#                 config.END_DATE)
+    klines_df = pd.DataFrame(data={ "timestamp":klines_np[:,0],
+                                    "open":klines_np[:,1],
+                                    "close":klines_np[:,4],
+                                    "high":klines_np[:,2],
+                                    "low":klines_np[:,3],
+                                    "volume":klines_np[:,5]})
+    stock = StockDataFrame.retype(klines_df)
+    add_history_db(stock)
 
-# klines_np = np.array(klines).astype('float64')
+def handle_socket_message(msg):
+    global klines_df
+    global BDB
+    print ("handle")
+    if msg['k']['x'] == True:
+        print ("Adding new data to RDS")
+        cur_df = pd.DataFrame( [[ float(j) for i,j in msg['k'].items() if i in ['t', 'o','c','h','l','v'] ]],
+                                columns=["timestamp", "open", "close", "high", "low", "volume"])
+        klines_df = klines_df.iloc[1:, :]
+        klines_df.append(cur_df, ignore_index=True)
+        stock = StockDataFrame.retype(klines_df)
+        stock['macd']
+        stock['rsi_6']
+        cur_df['macd'] = stock['macd'].iloc[-1]
+        cur_df['rsi_6'] = stock['rsi_6'].iloc[-1]
+        BDB.insert_data(cur_df['timestamp'].iloc[0],
+                        cur_df['open'].iloc[0],
+                        cur_df['high'].iloc[0],
+                        cur_df['low'].iloc[0],
+                        cur_df['close'].iloc[0],
+                        cur_df['volume'].iloc[0],
+                        cur_df['macd'].iloc[0],
+                        cur_df['rsi_6'].iloc[0])
+        
+        print("Added New data to RDS")
+        BDB.deleteFirstRow()
+        BDB.commit()
 
-# klines_df = pd.DataFrame(data={ "timestamp":klines_np[:,0],
-#                                 "open":klines_np[:,1],
-#                                 "close":klines_np[:,4],
-#                                 "high":klines_np[:,2],
-#                                 "low":klines_np[:,3],
-#                                 "volume":klines_np[:,5],
-#                                 "amount":klines_np[:,8] 
-#                                })
+def add_history_db(stock):
+    print ("Started: adding historical data")
+    stock['macd']
+    stock['rsi_6']
+    for i, row in stock.iterrows():
+        if row.isnull().any():
+            continue
+        BDB.insert_data(stock['timestamp'].iloc[i],
+                        stock['open'].iloc[i],
+                        stock['high'].iloc[i],
+                        stock['low'].iloc[i],
+                        stock['close'].iloc[i],
+                        stock['volume'].iloc[i],
+                        stock['macd'].iloc[i],
+                        stock['rsi_6'].iloc[i])
+    BDB.commit()
+    print ("complated : Adding historical data (Added %s rows)" % (stock.size))
 
-# def main():
+def main():
+    global klines_df
+    binance_api_key = config.API_KEY
+    binance_api_secret = config.API_SECRET
+    binance_client = Client(binance_api_key, binance_api_secret)
 
-#     twm = ThreadedWebsocketManager(api_key=binance_api_key, api_secret=binance_api_secret)
-#     # start is required to initialise its internal loop
-#     twm.start()
+    get_historical_data(binance_client)
 
-#     def handle_socket_message(msg):
-#         global klines_df
-#         new_df = pd.DataFrame( [[ float(j) for i,j in msg['k'].items() if i in ['t', 'o','c','h','l','v'] ]],
-#                              columns=["timestamp", "open", "close", "high", "low", "volume"])
-#         klines_df = klines_df.iloc[1: , :]
-#         klines_df.append(new_df, ignore_index=True)
-#         stock = StockDataFrame.retype(klines_df)
-#         new_df['macd'] = stock['macd'].iloc[-1]
-#         new_df['rsi_6'] = stock['rsi_6'].iloc[-1]
-#         print (new_df)
-#         print ("-----")
+    twm = ThreadedWebsocketManager(api_key=binance_api_key, api_secret=binance_api_secret)
+    # start is required to initialise its internal loop
+    twm.start()
 
-#     twm.start_kline_socket(callback=handle_socket_message, symbol=config.SYMBOL)
+    twm.start_kline_socket(callback=handle_socket_message, symbol=config.SYMBOL, interval='30m')
+    twm.join()
 
 
-#     twm.join()
-
-
-# if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+   main()
 
